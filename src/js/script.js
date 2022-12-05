a82707e95be2aeac50a85e97f8f52b30ebb2ebc2
Dawid <daw.parc@gmail.com>
2022-12-05 19:11:58 +0100

    Add changing images

--- src/js/script.js
+++ src/js/script.js
@@ -93,6 +93,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -165,10 +166,12 @@
 
           /* determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true } */
           const option = param.options[optionId];
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          const optionSelected = formData [paramId] && formData[paramId].includes(optionId);
           console.log(optionId, option);
 
           /* check if there is param with a name of paramId in formData and if it includes optionId */
-          if (formData [paramId] && formData[paramId].includes(optionId)){
+          if (optionSelected){
 
             /* chceck if the option is not default */
             if(!option.default){
@@ -182,6 +185,16 @@
 
             /* reduce price variable */
             price -= option.price;
+          }
+
+          if (optionImage){
+
+            if (optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+    
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
           }
         }
       }
