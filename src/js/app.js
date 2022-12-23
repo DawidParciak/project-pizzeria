e7697f22c5ba2148ed8c92830b14ac487a25470f
Dawid <daw.parc@gmail.com>
2022-12-23 20:11:13 +0100

    Add Booking.js (10.4)

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames } from './settings.js';
 import Product from './components/product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
@@ -93,12 +94,20 @@ const app = {
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingElement = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingElement);
+  },
+
   init: function(){
     const thisApp = this;
 
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,36 @@
+import { select, templates } from '../settings.js';
+import AmountWidget from '../components/AmountWidget.js';
+
+class Booking{
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+    
+    thisBooking.dom.wrapper = generatedHTML;
+
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+
+}
+
+export default Booking;
