f8df088ba25eed4765bde20322a8f27615a5c0a9
Dawid <daw.parc@gmail.com>
2023-01-14 12:36:40 +0100

    Remove bugs in server.mjs

--- server.mjs
+++ server.mjs
@@ -2,11 +2,12 @@ import path from 'path';
 import jsonServer from 'json-server';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json');
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
